902a81439d6db67d6bd9671e2d9d9c0753f0c616
whyorean <whyorean@gmail.com>
2017-01-07 00:12:55 +0530

    cpufreq: Add Darkness Governer
     - credits @Alucard_24
    
    Signed-off-by: Mihran Thalhath <mihranz7@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -557,6 +557,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_TRIPNDROID)
 extern struct cpufreq_governor cpufreq_gov_tripndroid;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_tripndroid)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
