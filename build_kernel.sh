3bf84497d4b89d5a1d5d69a257426e7ff238e4e8
shahan_mik3 <shahan.mike13@gmail.com>
2018-05-23 15:57:13 +0530

    Add kernel build script

---
+++ build_kernel.sh
@@ -0,0 +1,22 @@
+DEFCONFIG=whyred-perf_defconfig
+OBJ_DIR=`pwd`/.obj
+ANYKERNEL_DIR=${HOME}/ak
+TOOLCHAIN=${HOME}/tc/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
+WEB_SERVER_ROOT=/var/www/html
+DATE=$(date +"%m-%d-%y")
+
+if [ ! -d ${OBJ_DIR} ]; then
+    mkdir ${OBJ_DIR}
+fi
+
+make ARCH=arm64 O=$OBJ_DIR CROSS_COMPILE=${TOOLCHAIN} $DEFCONFIG
+make -j$(grep -c ^processor /proc/cpuinfo) ARCH=arm64 O=$OBJ_DIR CROSS_COMPILE=${TOOLCHAIN}
+rm -f ${ANYKERNEL_DIR}/Image.gz*
+rm -f ${ANYKERNEL_DIR}/zImage*
+rm -f ${ANYKERNEL_DIR}/dtb*
+cp $OBJ_DIR/arch/arm64/boot/Image.gz-dtb ${ANYKERNEL_DIR}/zImage-dtb
+cp $OBJ_DIR/drivers/staging/qcacld-3.0/wlan.ko ${ANYKERNEL_DIR}/modules/qca_cld3_wlan.ko
+cd ${ANYKERNEL_DIR}
+rm *.zip
+zip -r9 ZeurionX-$DATE.zip * -x README ZeurionX-$DATE.zip
+cp Z* ${WEB_SERVER_ROOT}
